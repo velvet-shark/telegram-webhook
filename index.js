7b2b2b7f5ac2741d858140b03f6f24fa3bef7ba6
Radek Sienkiewicz <mail@velvetshark.com>
2023-05-04 12:48:55 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const chatId = process.env.TELEGRAM_CHAT_ID;
+
+const bot = new TelegramBot(token, { polling: false });
+
+const app = express();
+app.use(bodyParser.json()); // Parse JSON request bodies
+
+// Webhook endpoint
+app.post("/webhook", (req, res) => {
+  console.log("Webhook triggered:", req.body);
+
+  const message = `Webhook triggered:\n\n${JSON.stringify(req.body, null, 2)}`;
+
+  bot
+    .sendMessage(chatId, message)
+    .then(() => {
+      console.log("Telegram notification sent");
+      res.sendStatus(200); // Acknowledge receipt of the webhook
+    })
+    .catch((error) => {
+      console.error("Error sending Telegram notification:", error);
+      res.sendStatus(500); // Internal Server Error
+    });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Webhook listener running on port ${port}`);
+});
