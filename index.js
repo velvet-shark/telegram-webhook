2d7b7b6e5e67a009d95599fc3be600294c624c74
Radek Sienkiewicz <mail@velvetshark.com>
2023-05-04 17:12:12 +0200

    Sending messages based on to/from

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post("/", (req, res) => {
   //   msgBody.value
   // }\n\nDetails: https://goerli.etherscan.io/tx/${msgBody.hash} `;
 
-  if (msgBody.from == "0x498098ca1b7447fc5035f95b80be97ee16f82597") {
+  if (JSON.stringify(msgBody).from == "0x498098ca1b7447fc5035f95b80be97ee16f82597") {
     message = `ETH sent from my wallet!\nTo: https://goerli.etherscan.io/address/${msgBody.to}\nAmount: ${(
       Number.parseInt(msgBody.value) / 1e18
     ).toLocaleString(undefined, { minimumFractionDigits: 8 })} ETH\n\nDetails: https://goerli.etherscan.io/tx/${
@@ -30,7 +30,7 @@ app.post("/", (req, res) => {
     }`;
   }
 
-  if (JSON.stringify(msgBody)[0].to == "0x498098ca1b7447fc5035f95b80be97ee16f82597") {
+  if (JSON.stringify(msgBody).to == "0x498098ca1b7447fc5035f95b80be97ee16f82597") {
     message = `ETH sent to my wallet!\nFrom: https://goerli.etherscan.io/address/${msgBody.from}\nAmount: ${(
       Number.parseInt(msgBody.value) / 1e18
     ).toLocaleString(undefined, { minimumFractionDigits: 8 })} ETH\n\nDetails: https://goerli.etherscan.io/tx/${
