d9ff9f1cc70b6d5fb47ac74a33d8c0665e238825
Radek Sienkiewicz <mail@velvetshark.com>
2023-05-04 16:53:21 +0200

    Sending messages based on to/from

--- index.js
+++ index.js
@@ -23,11 +23,15 @@ app.post("/", (req, res) => {
   // }\n\nDetails: https://goerli.etherscan.io/tx/${msgBody.hash} `;
 
   if (msgBody.from == "0x498098ca1b7447fc5035f95b80be97ee16f82597") {
-    message = `ETH sent from my wallet!\nTo:${msgBody.to}\nAmount: ${msgBody.value} ETH\n\nDetails: https://goerli.etherscan.io/tx/${msgBody.hash}`;
+    message = `ETH sent from my wallet!\nTo: https://goerli.etherscan.io/address/${
+      msgBody.to
+    }\nAmount: ${Number.parseInt(msgBody.value, 8)} ETH\n\nDetails: https://goerli.etherscan.io/tx/${msgBody.hash}`;
   }
 
   if (JSON.stringify(msgBody)[0].to == "0x498098ca1b7447fc5035f95b80be97ee16f82597") {
-    message = `ETH sent to my wallet!\nFrom:${msgBody.from}\nAmount: ${JmsgBod.value} ETH\n\nDetails: https://goerli.etherscan.io/tx/${msgBody.hash}`;
+    message = `ETH sent to my wallet!\nFrom: https://goerli.etherscan.io/address/${
+      msgBody.from
+    }\nAmount: ${Number.parseInt(msgBody.value, 8)} ETH\n\nDetails: https://goerli.etherscan.io/tx/${msgBody.hash}`;
   }
 
   bot
