ea155071cb997402a1ae155de90b94207195ebe4
Radek Sienkiewicz <mail@velvetshark.com>
2023-05-04 16:11:30 +0200

    Sending messages based on to/from

--- index.js
+++ index.js
@@ -13,11 +13,19 @@ app.use(bodyParser.json()); // Parse JSON request bodies
 
 // Webhook endpoint
 app.post("/", (req, res) => {
-  msgBody = req.body;
+  msgBody = JSON.stringify(req.body);
 
   console.log("Webhook triggered:", JSON.stringify(msgBody));
 
-  const message = `Webhook triggered:\n\n${JSON.stringify(msgBody)}`;
+  // const message = `Webhook triggered:\n\n${msgBody}`;
+
+  if msgBody.from == "0x498098ca1b7447fc5035f95b80be97ee16f82597" {
+    message = `ETH sent from my wallet!\nTo:${msgBody.to}\nAmount: ${msgBody.value} ETH\n\nDetails: https://goerli.etherscan.io/tx/${msgBody.hash}`;
+  }
+
+  if msgBody.to == "0x498098ca1b7447fc5035f95b80be97ee16f82597" {
+    message = `ETH sent to my wallet!\nFrom:${msgBody.from}\nAmount: ${msgBody.value} ETH\n\nDetails: https://goerli.etherscan.io/tx/${msgBody.hash}`;
+  }
 
   bot
     .sendMessage(chatId, message)
